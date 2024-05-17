38002d3e756255f40e9171559b2ee62133bf8238
kinxyzz <kinxyzz@gmail.com>
2024-05-18 02:44:09 +0700

    feat:add lazyload at router component'

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { Flip, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import ExploreForYou from './components/template/explore/ExploreForYou';
-import ExploreTrending from './components/template/explore/ExploreTrending';
-import Beranda from './pages/beranda/Beranda';
-import Message from './pages/beranda/Message';
-import Explore from './pages/beranda/explore/Explore';
-import MainContent from './pages/beranda/story/MainContent';
-import Login from './pages/login/index';
-import Signup from './pages/register/index';
-import SetProfile from './pages/register/set-profile';
+import Loading from './components/element/Loading';
 
 const queryClient = new QueryClient();
+
+const Login = lazy(() => import('./pages/login/index'));
+const Signup = lazy(() => import('./pages/register/index'));
+const SetProfile = lazy(() => import('./pages/register/set-profile/index'));
+const Beranda = lazy(() => import('./pages/beranda/Beranda'));
+const Explore = lazy(() => import('./pages/beranda/explore/Explore'));
+const MainContent = lazy(() => import('./pages/beranda/story/MainContent'));
+const Message = lazy(() => import('./pages/beranda/Message'));
+const ExploreTrending = lazy(
+  () => import('./components/template/explore/ExploreTrending'),
+);
+const ExploreForYou = lazy(
+  () => import('./components/template/explore/ExploreForYou'),
+);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Outlet />}>
